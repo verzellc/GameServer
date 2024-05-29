00fe097e1188db4991b4c863780478899d8ec309
Finallz <zhunling@gmail.com>
2024-05-29 16:06:00 -0700

    Initial commit

---
+++ TcpClient/Program.cs
@@ -0,0 +1,56 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+
+var hostName = Dns.GetHostName();
+IPHostEntry localhost = await Dns.GetHostEntryAsync(hostName);
+// This is the IP address of the local machine
+IPAddress localIpAddress = localhost.AddressList[0];
+IPEndPoint ipEndPoint = new(localIpAddress , 13000);
+using Socket client = new(
+ipEndPoint.AddressFamily, 
+SocketType.Stream, 
+ProtocolType.Tcp);
+
+try 
+{
+    await client.ConnectAsync(ipEndPoint);
+    Console.WriteLine($"Server connected: {client.RemoteEndPoint}.");
+    while (true)
+    {
+        String? message = Console.ReadLine();
+        if (String.IsNullOrEmpty(message))
+        {
+            continue;
+        }
+
+        var messageBytes = Encoding.UTF8.GetBytes(message);
+        _ = await client.SendAsync(messageBytes, SocketFlags.None);
+        Console.WriteLine($"Socket client sent message: \"{message}\"");
+
+        // Receive ack.
+        var buffer = new byte[1_024];
+        var received = await client.ReceiveAsync(buffer, SocketFlags.None);
+        var response = Encoding.UTF8.GetString(buffer, 0, received);
+        if (response == "<|ACK|>")
+        {
+            Console.WriteLine(
+                $"Socket client received acknowledgment: \"{response}\"");
+        }
+
+        if (message.Equals("close", StringComparison.InvariantCultureIgnoreCase))
+        {
+            break;
+        }
+    }
+}
+catch (Exception e) 
+{
+    Console.WriteLine("Error..... " + e.StackTrace);
+}
+finally
+{
+    client.Shutdown(SocketShutdown.Both);
+    client.Close();
+}
---
+++ TcpServer/Program.cs
@@ -0,0 +1,67 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+var hostName = Dns.GetHostName();
+IPHostEntry localhost = await Dns.GetHostEntryAsync(hostName);
+// This is the IP address of the local machine
+IPAddress localIpAddress = localhost.AddressList[0];
+int port = 13000;
+IPEndPoint ipEndPoint = new(localIpAddress , port);
+using Socket listener = new(
+    ipEndPoint.AddressFamily,
+    SocketType.Stream,
+    ProtocolType.Tcp);
+listener.Bind(ipEndPoint);
+listener.Listen(100);
+Console.WriteLine($"Listening on port {port}.");
+
+try
+{   
+    while (true)
+    {
+        Socket handler = await listener.AcceptAsync();
+        ThreadPool.QueueUserWorkItem(state => HandleConnection(handler));
+    }
+}
+catch (Exception e)
+{
+    Console.WriteLine(e.ToString());
+}
+finally
+{
+    listener.Shutdown(SocketShutdown.Both);
+}
+
+async void HandleConnection(Socket handler)
+{
+    try 
+    {
+        Console.WriteLine($"Client connected: {handler.RemoteEndPoint}.");
+        while (true)
+        {
+            var buffer = new byte[1_024];
+            var received = await handler.ReceiveAsync(buffer, SocketFlags.None);
+            var receivedMessage = Encoding.UTF8.GetString(buffer, 0, received);
+
+            Console.WriteLine(
+                $"Socket server received message: \"{receivedMessage}\"");
+            if (String.IsNullOrEmpty(receivedMessage))
+            {
+                Console.WriteLine($"Disconnect from client: {handler.RemoteEndPoint}.");
+                handler.Shutdown(SocketShutdown.Both);
+                break;
+            }
+
+            var ackMessage = "<|ACK|>";
+            var echoBytes = Encoding.UTF8.GetBytes(ackMessage);
+            await handler.SendAsync(echoBytes, 0);
+            Console.WriteLine(
+                $"Socket server sent acknowledgment: \"{ackMessage}\"");
+        }
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine(e.ToString());
+    }
+}
